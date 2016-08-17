f4d23f5166df8319eaa9dbea64d7c83f1eb50b87
Madhu <s.madhuvani@gmail.com>
2016-08-17 12:04:59 +0530

    constructor

---
+++ corejava1/src/Contructorsample.java
@@ -0,0 +1,21 @@
+
+public class Contructorsample {
+
+	/**
+	 * @param args
+	 * 
+	 * 
+	 */
+	static int i;
+	Contructorsample(){
+		i=10;
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Contructorsample sample=new Contructorsample();
+		System.out.println("value of i: "+i);
+	}
+
+}
